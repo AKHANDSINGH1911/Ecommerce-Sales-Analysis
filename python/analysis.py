845c6ade4d4f528249ff5cb10e80ca4488597bca
AKHANDSINGH1911 <akhandsingh191102@gmail.com>
2026-02-24 02:01:28 +0530

    change in code

--- python/analysis.py
+++ python/analysis.py
@@ -18,16 +18,7 @@ df = pd.read_sql(query, conn)
 print("Rows:", df.shape)
 
  # convert date
-# df['orderDate'] = pd.to_datetime(df['orderDate'])
-# df['month'] = df['orderDate'].dt.month
-
-# convert orderDate safely
-df['orderDate'] = pd.to_datetime(df['orderDate'], errors='coerce')
-
-# remove invalid dates
-df = df.dropna(subset=['orderDate'])
-
-# extract month
+df['orderDate'] = pd.to_datetime(df['orderDate'])
 df['month'] = df['orderDate'].dt.month
 
 # check result
@@ -52,21 +43,10 @@ plt.title("Product Sales")
 plt.savefig("../excel/product_sales_chart.png")
 
  # export excel
-# with pd.ExcelWriter("../excel/analysis_output.xlsx") as writer:
-#     monthly_sales.to_excel(writer, sheet_name="Monthly Sales")
-#     top_customers.to_excel(writer, sheet_name="Top Customers")
-#     product_sales.to_excel(writer, sheet_name="Product Sales")
-#     country_sales.to_excel(writer, sheet_name="Country Sales")
-
-# convert series to dataframe before saving
-monthly_sales_df = monthly_sales.reset_index()
-top_customers_df = top_customers.reset_index()
-product_sales_df = product_sales.reset_index()
-country_sales_df = country_sales.reset_index()
-
 with pd.ExcelWriter("../excel/analysis_output.xlsx") as writer:
-    monthly_sales_df.to_excel(writer, sheet_name="Monthly Sales", index=False)
-    top_customers_df.to_excel(writer, sheet_name="Top Customers", index=False)
-    product_sales_df.to_excel(writer, sheet_name="Product Sales", index=False)
-    country_sales_df.to_excel(writer, sheet_name="Country Sales", index=False)
+    monthly_sales.to_excel(writer, sheet_name="Monthly Sales")
+    top_customers.to_excel(writer, sheet_name="Top Customers")
+    product_sales.to_excel(writer, sheet_name="Product Sales")
+    country_sales.to_excel(writer, sheet_name="Country Sales")
+
 print("Analysis complete")
